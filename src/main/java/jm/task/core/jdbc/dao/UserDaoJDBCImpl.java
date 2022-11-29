8358fd1eef4ca9215be7b976aa9f170d63502345
Mxm06 <stingermax06@mail.ru>
2022-11-29 22:49:19 +0500

    1.1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     private long id = 1;
-    private final String INSERT_NEW = "INSERT INTO users VALUES(?,?,?,?)";
+    private final String INSERT_NEW = "INSERT INTO users (name,lastname,age) VALUES(?,?,?)";
     private final String DELETE_ID = "DELETE FROM Users WHERE id = ?";
     private final String GET_ALL = "SELECT * FROM Users";
 
@@ -19,32 +19,31 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute("CREATE TABLE `firstdb`.`Users` (\n" +
-                    "  `id` BIGINT(1) NOT NULL,\n" +
+            statement.execute("CREATE TABLE IF NOT EXISTS `firstdb`.`Users` (\n" +
+                    "  `id` BIGINT(1) NOT NULL AUTO_INCREMENT,\n" +
                     "  `name` VARCHAR(45) NOT NULL,\n" +
                     "  `lastName` VARCHAR(45) NOT NULL,\n" +
                     "  `age` TINYINT(1) NOT NULL,\n" +
                     "  PRIMARY KEY (`id`));\n");
         } catch (SQLException e) {
-            System.out.println("Таблица уже существует");
+            e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
         try (Statement statement = Util.getConnection().createStatement()) {
-            statement.execute("DROP TABLE Users");
-        } catch (SQLException throwables) {
-            System.out.println("Таблицы не существует");
+            statement.execute("DROP TABLE IF EXISTS Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
 
     public void saveUser(String name, String lastName, byte age) {
         try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(INSERT_NEW)) {
-            preparedStatement.setLong(1, id++);
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, lastName);
-            preparedStatement.setByte(4, age);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
             preparedStatement.execute();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -62,8 +61,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> userList = new LinkedList<>();
-        try (Statement statement = Util.getConnection().createStatement()) {
-            ResultSet resultSet = statement.executeQuery(GET_ALL);
+        try (PreparedStatement statement = Util.getConnection().prepareStatement(GET_ALL)) {
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 userList.add(new User(resultSet.getString(2),resultSet.getString(3),resultSet.getByte(4)));
             }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,11 +7,15 @@ import java.sql.Statement;
 
 
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/firstdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ardashov06";
+
     static private Connection connection;
 
     static {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/firstdb", "root", "Ardashov06");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
